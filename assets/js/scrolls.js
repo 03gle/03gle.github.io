cecd9ec571cb1c48990d6918984bcc704cb914b8
Brin-o <49071768+Brin-o@users.noreply.github.com>
2022-05-28 12:49:18 +0200

    header fix

---
+++ assets/js/scrolls.js
@@ -0,0 +1,42 @@
+window.addEventListener("scroll", function (event) {
+    var scroll = this.scrollY;
+
+    var header = this.document.getElementById("header")
+    var navItems = this.document.getElementById("nav").children;
+    var name = this.document.getElementById("personName")
+
+    var r = document.querySelector(':root');
+    var rs = getComputedStyle(r)
+
+    var scrollGate = rs.getPropertyValue("--header-stick-after-pixels") //starts transition when you passed this amount of pixels
+
+    var lightColor = rs.getPropertyValue("--main-bg-color")
+    var darkColor = rs.getPropertyValue("--dark-text-color")
+
+    //console.log(scroll)
+    if (scroll > scrollGate) {
+        header.style.backgroundColor = lightColor
+        header.style.color = darkColor
+        header.style.position = "sticky";
+
+
+        //change the color of the name
+        name.style.color = darkColor
+        //change link color
+        for (let item of navItems) {
+            item.style.color = darkColor
+        }
+    }
+
+    else {
+        header.style.backgroundColor = "transparent"
+        header.style.color = lightColor
+        header.style.position = "absolute";
+
+        name.style.color = lightColor
+        //change link color
+        for (let item of navItems) {
+            item.style.color = lightColor
+        }
+    }
+})
